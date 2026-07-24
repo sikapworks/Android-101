d313e6e8213d214c1f70da90dcfc9695bdae515c
sikape <ksiya1306@gmail.com>
2026-07-24 16:48:14 +0530

    made profile header

---
+++ app/src/main/java/uk/ac/tees/mad/learningapp/ProfileHeader.kt
@@ -0,0 +1,31 @@
+package uk.ac.tees.mad.learningapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+@Preview(showBackground = true)
+fun ProfileHeader() {
+    Column {
+        Text(
+            text = "Name: Siya Kapoor",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Role: Android Developer",
+            fontSize = 16.sp,
+            color = Color.Gray
+        )
+    }
+}
